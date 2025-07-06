075d045d8ae5ef4c3f20e043922f72e519d4027e
dubraska <solorzano.dubraska@gmail.com>
2025-07-06 15:28:11 -0400

    Solucion Hack 1

--- hack_1/hack_1_for.js
+++ hack_1/hack_1_for.js
@@ -8,9 +8,6 @@
 
 let result = [];
 
-// Variable de control
-let i = 0;
-
 
 for (let i = 0; i <= 5; i++) { // El for inializa i en 0, y se ejecuta mientras sea menor o igual a 5
   result.push(i); //se agrega el valor de i en cacda ciclo
