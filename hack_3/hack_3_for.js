509e9d357504d82602b7b899ab809bbfb2857cee
dubraska <solorzano.dubraska@gmail.com>
2025-07-06 16:16:17 -0400

    Solucion Hack 3

--- hack_3/hack_3_for.js
+++ hack_3/hack_3_for.js
@@ -7,6 +7,11 @@
  */
 let result = [];
 
+for(let i = 5; i >= 1; i--){ //la variable i se inicializa en 5, para empezar a disminuirla en 1 en cada ciclo (i--)
+    result.push(i);
+}
+
+console.log(result);
 
 //export result
 module.exports = result;
--- hack_3/hack_3_while.js
+++ hack_3/hack_3_while.js
@@ -7,6 +7,15 @@
  */
 let result = [];
 
+let i = 5; //sE INICIALIZA EN 5 para ir bajando
+
+while(i >= 1){
+    result.push(i);
+    i--; //Disminuimos i en 1 cada ciclo
+}
+
+console.log(result);
+
 
 //export result
 module.exports = result;
