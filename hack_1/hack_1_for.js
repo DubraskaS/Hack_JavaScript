93c5ce6096df5a94579cdd49b06a66c8971c8f1d
dubraska <solorzano.dubraska@gmail.com>
2025-07-06 15:25:48 -0400

    Solucion Hack 1

--- hack_1/hack_1_for.js
+++ hack_1/hack_1_for.js
@@ -5,8 +5,19 @@
  * 
  * output => [0,1,2,3,4,5]
  */
+
 let result = [];
 
+// Variable de control
+let i = 0;
+
+
+for (let i = 0; i <= 5; i++) { // El for inializa i en 0, y se ejecuta mientras sea menor o igual a 5
+  result.push(i); //se agrega el valor de i en cacda ciclo
+}
+
+// Corroborando el output
+console.log(result);
 
 //export result
 module.exports = result;
--- hack_1/hack_1_while.js
+++ hack_1/hack_1_while.js
@@ -7,6 +7,18 @@
  */
 let result = [];
 
+// Variable de control
+let i = 0;
+
+// El while se ejecuta mientras i sea menor o igual a 5
+while (i <= 5) {
+  result.push(i); //se agrega el valor de i en cacda ciclo
+  // Incrementamos el valor de 'i' 
+  i++;
+}
+
+// Corroborando el output
+console.log(result);
 
 //export result
 module.exports = result;
