4331e20e1089e0439f17c541fc5a72d232f6dcc2
dubraska <solorzano.dubraska@gmail.com>
2025-07-14 17:06:35 -0400

    Solucion Hack 7

--- hack_7/hack_7.js
+++ hack_7/hack_7.js
@@ -9,6 +9,23 @@
 let arr = ["foo","bar","baz","qux","echo"];
 let result = [];
 
+//USANDO MAP
+
+result = arr.map(item => {
+  // "item" representa cada string contenido en arr
+
+  //Tomando el primer caracter del string y convirtiendolo a mayúscula
+  const first = item[0].toUpperCase();
+
+  //Tomando el resto del string (desde el segundo caracter en adelante)
+  const resto = item.slice(1);
+
+  //Uniendo el primer caracter capitalizado con el resto del string
+  return `${first}${resto}`;
+});
+
+console.log(result);
+
 
 //export result
 module.exports = result;
