0a88fb060249284440a28483047f7ad8116bbcdc
dubraska <solorzano.dubraska@gmail.com>
2025-07-14 21:56:26 -0400

    Solucion Hack 9

--- hack_9/hack_9.js
+++ hack_9/hack_9.js
@@ -8,7 +8,14 @@
  */
 let arr = ["foo","bar","baz","qux","echo"];
 let result = 0;
+let mapping = [];
 
+for(let i = 0; i < arr.length; i++){
+    if (arr[i][0] === 'b'){
+        result++;
+    }
+}
 
+console.log(result);
 //export result
 module.exports = result;
