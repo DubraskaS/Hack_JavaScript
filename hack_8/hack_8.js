76eb24c21f4cb70d00d42602801659f0fc676e90
dubraska <solorzano.dubraska@gmail.com>
2025-07-14 21:47:51 -0400

    Solucion Hack 8

--- hack_8/hack_8.js
+++ hack_8/hack_8.js
@@ -11,6 +11,24 @@
 let arr = ["foo","bar","baz","qux","echo"];
 let result = [];
 
+//USANDO MAP
+
+//Anexamos a result los elementos [bar,baz,qux] del array de entrada (usando slice para cortar solo esa parte del array)
+let tempResult = arr.slice(1, 4); // tempResult ahora es ["bar", "baz", "qux"]
+
+//Los 2 primeros elementos, reescribir "a" por "@" (iteramos usando MAP)
+result = tempResult.map((item, index) => {
+  if (index === 0 || index === 1) {
+    //Si es el primer o segundo elemento, reemplazamos 'a' por '@'
+    return item.replace('a', '@');
+  } else {
+    return item;
+  }
+});
+//Modificamos el último ítem "quz" a mayúscula
+result[2] = result[2].toUpperCase(); 
+
+console.log(result);
 
 //export result
 module.exports = result;
